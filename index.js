ad2d4f683400257764bd871f72106d9e78a20b8a
Milena-Arantes <milenaarantes091@gmail.com>
2024-03-02 12:01:52 -0300

    Iniciando JavaScript

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Olá mundo');
+
+const a = 10.5;
+let b = 20;
+let nome = "Milena";
+
+console.log(nome);
+
+const total = a + b;
+console.log("O total é: " + total);
+console.log(`Total: ${total}`)
